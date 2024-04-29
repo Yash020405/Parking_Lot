fd500004d3aeab4681a3132708f9d0b62c4e121a
Yash020405 <yashaga0204@gmail.com>
2024-04-29 20:48:05 +0530

    Parking Lot commit

---
+++ src/main/java/org/example/controllers/TicketController.java
@@ -0,0 +1,12 @@
+package org.example.controllers;
+
+import org.example.dtos.IssueTicketRequestDto;
+import org.example.dtos.IssueTicketResponseDto;
+
+public class TicketController {
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto) {
+
+        return new IssueTicketResponseDto();
+
+    }
+}
